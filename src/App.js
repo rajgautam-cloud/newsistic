451c6e01a01c2fbb336c87d6ce6e6a201d0debb2
gautamRaj-cloud <raj350199@gmail.com>
2021-08-23 23:10:50 +0530

    created react app and added basic files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+const alanKey =
+  "3d3fad30ca9c981b2a979fc63459bfa32e956eca572e1d8b807a3e2338fdd0dc/stage";
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This code was executed");
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>News Application</h1>
     </div>
   );
-}
-
+};
 export default App;
