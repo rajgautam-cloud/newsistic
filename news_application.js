cc1d6b8225a55f90cc62cb29182b75fb07902d37
rajgautam-cloud <75833074+rajgautam-cloud@users.noreply.github.com>
2021-08-24 11:27:34 +0530

    Added command to get news from sources

--- news_application.js
+++ news_application.js
@@ -1,16 +1,31 @@
-intent('What does this app do?','What can I do here?','Tell me about this app.',
-      reply('This is an news reading application.'));
-// intent('Start a command',(p)=>{
-//     p.play({command:'testCommand'});
-// })
-const API_KEY='a085a9957ef1464694a33ae75ea90212';
 
-intent('Give me the news from $(source* (.*))',(p)=>{
-    let NEWS_API_URL=`https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
+
+intent('What does this app do?', 'What can I do here?', 
+      reply('This is a news project.'));
+
+
+const API_KEY = 'a085a9957ef1464694a33ae75ea90212';
+let savedArticles = [];
+
+
+// News by Source
+intent('Give me the news from $(source* (.*))', (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
     
-    
-    if(p.source.value){
-        NEWS_API_URL=`${NEWS_API_URL}&source=${p.source.value.toLowercase().split(" ").join('-')}`
+    if(p.source.value) {
+        NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
     }
-
+    
+    api.request(NEWS_API_URL, (error, response, body) => {
+        const { articles } = JSON.parse(body);
+        
+        if(!articles.length) {
+            p.play('Sorry, please try searching for news from a different source');
+            return;
+        }
+        savedArticles=articles;
+        p.play({command:'newHeadlines',articles});
+        p.play(`Here are the (latest|recent) ${p.source.value} news`);
+        
+       });
 })
