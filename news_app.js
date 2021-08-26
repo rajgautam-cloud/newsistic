df4912e051923c1a377a1eb83568c5cc4f5dfd93
gautamRaj-cloud <raj350199@gmail.com>
2021-08-26 22:38:21 +0530

    Modified news.js to get news from categories and keyword

--- news_app.js
+++ news_app.js
@@ -30,11 +30,14 @@ intent("Give me the news from $(source* (.*))", (p) => {
 
     p.play({ command: "newHeadlines", articles });
     p.play(`Here are the (latest|recent) ${p.source.value}.`);
+
+    p.play("Would you like me to read the headlines?");
+    p.then(confirmation);
   });
 });
 
-//News by Term
-intent("what 's up with $(term* (.*))", (p) => {
+// News by Term
+intent("what's up with $(term* (.*))", (p) => {
   let NEWS_API_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}`;
 
   if (p.term.value) {
@@ -53,5 +56,84 @@ intent("what 's up with $(term* (.*))", (p) => {
 
     p.play({ command: "newHeadlines", articles });
     p.play(`Here are the (latest|recent) articles on ${p.term.value}.`);
+
+    p.play("Would you like me to read the headlines?");
+    p.then(confirmation);
   });
+});
+
+// News by Categories
+const CATEGORIES = [
+  "business",
+  "entertainment",
+  "general",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+const CATEGORIES_INTENT = `${CATEGORIES.map(
+  (category) => `${category}~${category}`
+).join("|")}|`;
+
+intent(
+  `(show|what is|tell me|what's|what are|what're|read) (the|) (recent|latest|) $(N news|headlines) (in|about|on|) $(C~ ${CATEGORIES_INTENT})`,
+  `(read|show|get|bring me|give me) (the|) (recent|latest) $(C~ ${CATEGORIES_INTENT}) $(N news|headlines)`,
+  (p) => {
+    let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}&country=us`;
+
+    if (p.C.value) {
+      NEWS_API_URL = `${NEWS_API_URL}&category=${p.C.value}`;
+    }
+
+    api.request(NEWS_API_URL, (error, response, body) => {
+      const { articles } = JSON.parse(body);
+
+      if (!articles.length) {
+        p.play("Sorry, please try searching for a different category.");
+        return;
+      }
+
+      savedArticles = articles;
+
+      p.play({ command: "newHeadlines", articles });
+
+      if (p.C.value) {
+        p.play(`Here are the (latest|recent) articles on ${p.C.value}.`);
+      } else {
+        p.play(`Here are the (latest|recent) news`);
+      }
+
+      p.play("Would you like me to read the headlines?");
+      p.then(confirmation);
+    });
+  }
+);
+
+const confirmation = context(() => {
+  intent("yes", async (p) => {
+    for (let i = 0; i < savedArticles.length; i++) {
+      p.play({ command: "highlight", article: savedArticles[i] });
+      p.play(`${savedArticles[i].title}`);
+    }
+  });
+
+  intent("no", (p) => {
+    p.play("Sure, sounds good to me.");
+  });
+});
+
+intent("open (the|) (article|) (number|) $(number* (.*))", (p) => {
+  if (p.number.value) {
+    p.play({
+      command: "open",
+      number: p.number.value,
+      articles: savedArticles,
+    });
+  }
+});
+
+intent("(go|) back", (p) => {
+  p.play("Sure, going back");
+  p.play({ command: "newHeadlines", articles: [] });
 });
