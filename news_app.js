92ae604a724475982916b13a7692ca161c0f0cf7
gautamRaj-cloud <raj350199@gmail.com>
2021-08-26 14:46:53 +0530

    added new_app.js

--- news_app.js
+++ news_app.js
@@ -7,7 +7,6 @@ intent(
 const API_KEY = "a085a9957ef1464694a33ae75ea90212";
 let savedArticles = [];
 
-// News by Source
 intent("Give me the news from $(source* (.*))", (p) => {
   let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
 
