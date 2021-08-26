57eb1c57bc5616337b88fc61b116cab72a5bdbea
gautamRaj-cloud <raj350199@gmail.com>
2021-08-26 17:26:44 +0530

    changed background color,added alan ai logo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import NewsCards from "./components/NewsCards/NewsCards.component";
+import classNames from "classnames";
+import useStyles from "./styles";
+import AlanLogo from "./AlanLogo.png";
 
 const alanKey =
   "6cf38ce81e77c99e2a979fc63459bfa32e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
-
+  const classes = useStyles();
   useEffect(() => {
     alanBtn({
       key: alanKey,
@@ -22,7 +25,9 @@ const App = () => {
   }, []);
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        <img src={AlanLogo} className={classes.alanLogo} alt="Alan Logo" />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
