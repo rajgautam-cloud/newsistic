6ae70ce298a574bb88ecb085d316c9ed1d0e1441
gautamRaj-cloud <raj350199@gmail.com>
2021-08-28 13:42:42 +0530

    added react routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const Bookmarks = () => (
+  <div>
+    <h1>Bookmarks </h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/bookmarks" component={Bookmarks} />
       </Switch>
     </div>
   );
