fa95b47a0b61979fe909318257688ac12178627a
gautamRaj-cloud <raj350199@gmail.com>
2021-09-02 11:09:57 +0530

    changed color combination

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,19 +1,19 @@
 export const infoCards = [
-  { color: "#00838f", title: "Latest News", text: "Give me the latest news" },
+  { color: "#55BCC9", title: "Latest News", text: "Give me the latest news" },
   {
-    color: "#1565c0",
+    color: "#97CAEF",
     title: "News by Categories",
     info: "Business, Health, Science, Business , Sports, Technology",
     text: "Give me the latest Technology news",
   },
   {
-    color: "#4527a0",
+    color: "#97CAEF",
     title: "News by Terms",
     info: "Bitcoin, PlayStation 5, Smartphones, Donald Trump...",
     text: "What's up with PlayStation 5",
   },
   {
-    color: "#283593",
+    color: "#55BCC9",
     title: "News by Sources",
     info: "CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...",
     text: "Give me the news from BBC News",
