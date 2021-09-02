a5d2e15e15a609b2198d1ebef0ac0949a3b8569a
gautamRaj-cloud <raj350199@gmail.com>
2021-09-02 12:56:11 +0530

    added directory component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,57 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
-import alanBtn from "@alan-ai/alan-sdk-web";
-import wordsToNumbers from "words-to-numbers";
+import Directory from "../../components/Directory/Directory.component";
 
-import NewsCards from "../../components/NewsCards/NewsCards.component";
-//import useStyles from "./homepage.styles";
+import "./homepage.styles.scss";
 
-const alanKey =
-  "6cf38ce81e77c99e2a979fc63459bfa32e956eca572e1d8b807a3e2338fdd0dc/stage";
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
 
-const Homepage = () => {
-  const [newsArticles, setNewsArticles] = useState([]);
-  const [activeArticle, setActiveArticle] = useState(-1);
-  //const classes = useStyles();
-  const alanBtnInstance = useRef(null);
-
-  useEffect(() => {
-    if (!alanBtnInstance.current) {
-      alanBtnInstance.current = alanBtn({
-        key: alanKey,
-        onCommand: ({ command, articles, number }) => {
-          if (command === "newHeadlines") {
-            setNewsArticles(articles);
-            setActiveArticle(-1);
-          } else if (command === "highlight") {
-            setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
-          } else if (command === "open") {
-            const parsedNumber =
-              number.length > 2
-                ? wordsToNumbers(number, { fuzzy: true })
-                : number;
-            const article = articles[parsedNumber - 1];
-
-            if (parsedNumber > articles.length) {
-              alanBtn().playText("Please try that again...");
-            } else if (article) {
-              window.open(article.url, "_blank");
-              alanBtn().playText("Opening...");
-            } else {
-              alanBtn().playText("Please try that again...");
-            }
-          }
-        },
-      });
-    }
-  }, []);
-  return (
-    <div>
-      {/* <div className={classes.logoContainer}>
-        <img src={AlanLogo} className={classes.alanLogo} alt="Alan Logo" />
-      </div> */}
-      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
-    </div>
-  );
-};
-export default Homepage;
+export default HomePage;
